30a4e2cb36320702ed6ecb76178597f66cd94814
mrxcyb3rsec <101532971+mrxcyb3rsec@users.noreply.github.com>
2026-03-26 10:16:28 +0530

    Fix import formatting in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+ from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import random
 from datetime import datetime, timedelta
